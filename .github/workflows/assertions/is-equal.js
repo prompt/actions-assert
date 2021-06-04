6d5f584917505f6b2785668a3533a41b2ea8cf11
Samuel Ryan <sam@samryan.co.uk>
2021-06-05 00:10:50 +0100

    docs: Explicitly describe purpose of integration test assertions

--- .github/workflows/assertions/is-equal.js
+++ .github/workflows/assertions/is-equal.js
@@ -1,3 +1,10 @@
+// This file (`is-equal.js`) is a simple example assertion used as part of the
+// integration tests for the Action, it is not an assertion to be used by
+// Action consumers.
+
+// Please see the documentation for available assertions
+// https://github.com/pr-mpt/actions-assert#assertions
+
 module.exports = function (expected, actual) {
   return {
     pass: (actual == expected),
