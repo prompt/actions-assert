78723390c50e6bd38912013d6c6e3595fa635c49
Samuel Ryan <sam@pr-mpt.com>
2022-10-27 21:15:35 +0100

    chore: Upgrade dependencies (`node` + `@actions/core`) (#65)

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,7 @@ import {env} from 'process'
 export function hasActionInput(name: string): boolean {
   const key = `INPUT_${name.replace(/ /g, '_').toUpperCase()}`
 
-  if (core.getBooleanInput('convert-empty-to-null') && env[key] == '') {
+  if (core.getBooleanInput('convert-empty-to-null') && env[key] === '') {
     return false
   }
 
